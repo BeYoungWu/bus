926d0797be7c473c3316af21c0c68a27c0abbaec
BeYoungWu <byungwoo97@naver.com>
2023-06-08 16:12:51 +0900

    검색기능 구현

--- app/src/main/java/com/example/bus/MainActivity.java
+++ app/src/main/java/com/example/bus/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,7 +16,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         setTitle("메인화면");
 
-
+        // 자동완성텍스트뷰
+        String[] items = { "광운대학교", "노원구민회관", "월계삼거리", "인덕삼거리", "중계역", "하계시영아파트", "하계역", "하계장미아파트" };
+        AutoCompleteTextView auto = (AutoCompleteTextView) findViewById(R.id.search);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, items);
+        auto.setAdapter(adapter);
 
     }
 }
