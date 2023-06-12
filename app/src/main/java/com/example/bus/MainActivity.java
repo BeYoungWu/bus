d68ee773003618a1901e6aa9dd88fb6c0549cb05
BeYoungWu <byungwoo97@naver.com>
2023-06-12 21:00:15 +0900

    하차예약 구현 완료

--- app/src/main/java/com/example/bus/MainActivity.java
+++ app/src/main/java/com/example/bus/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.bus;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
@@ -14,32 +17,59 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnRes, btnEval, dialogBtnRes, dialogBtnReturn, btnCard;
+    TextView arrive, dialogArrive;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setTitle("메인화면");
 
-        // 자동완성텍스트뷰
-        String[] items = { "광운대학교", "노원구민회관", "월계삼거리", "인덕삼거리", "중계역", "하계시영아파트", "하계역", "하계장미아파트" };
-        AutoCompleteTextView auto = (AutoCompleteTextView) findViewById(R.id.search);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, items);
-        auto.setAdapter(adapter);
+        btnRes = (Button) findViewById(R.id.btnRes);
+        btnEval = (Button) findViewById(R.id.btnEval);
+        btnRes = (Button) findViewById(R.id.btnRes);
+        arrive = (TextView) findViewById(R.id.arrive);
 
-        // 하차예약 이동
-        Button btnRes = (Button) findViewById(R.id.reservation);
         btnRes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView arrive = (TextView) findViewById(R.id.arrive);
-                Intent intent = new Intent(getApplicationContext(), ReservationActivity.class);
-                intent.putExtra("arrive", arrive.getText().toString());
-                startActivityForResult(intent, 0);
+
+                // 대화상자
+                View dialogView = getLayoutInflater().inflate(R.layout.reservation, null);
+
+                // 자동완성텍스트뷰
+                final String[] items = { "광운대학교", "노원구민회관", "월계삼거리", "인덕삼거리", "중계역", "하계시영아파트", "하계역", "하계장미아파트" };
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_dropdown_item_1line, items);
+                AutoCompleteTextView auto = (AutoCompleteTextView) dialogView.findViewById(R.id.dialogSearch);
+                auto.setAdapter(adapter);
+
+                // 대화상자
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setView(dialogView);
+                dlg.setNegativeButton("취소", null);
+                dlg.setPositiveButton("예약하기", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        arrive.setText("하차 : " + dialogArrive.getText().toString());
+                    }
+                });
+                dlg.show();
+
+                // 하차예약 (자동완성텍스트뷰에 값을 지정하면 TextView에도 값 넘기기)
+                auto.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        String selectedValue = (String) adapterView.getItemAtPosition(i);
+                        dialogArrive = (TextView) dialogView.findViewById(R.id.dialogArrive);
+                        dialogArrive.setText(selectedValue);
+                    }
+                });
             }
         });
 
         // 평가하기 이동
-        Button btnEval = (Button) findViewById(R.id.evaluation);
+        Button btnEval = (Button) findViewById(R.id.btnEval);
         btnEval.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
